68a32d8b9542cc4bdd19b793395ebbd33dbf9715
Vignesh <vigneshwork23@gmail.com>
2023-12-03 19:26:53 +0530

    Check commit - 1

--- KryptX-final/project_web3.0/client/src/components/Exchange.jsx
+++ KryptX-final/project_web3.0/client/src/components/Exchange.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import '../assets/css/Exchange.css';
+import { Navbar, Footer } from '.';
 
 const Exchange =()=>{
     return(
         <div>
-
+            < Navbar/>
         </div>
     )
 }
